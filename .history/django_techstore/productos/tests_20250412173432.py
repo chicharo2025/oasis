404c818c50d898730c564d2c5af862f4ce81124f
Cristian2025 <cristiancardonauribe2022@gmail.com>
2025-04-22 16:35:50 -0500

    Delete .history/django_techstore directory

--- .history/django_techstore/productos/tests_20250412173432.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-# Antes (si verificabas producto.html)
-self.assertTemplateUsed(response, 'productos/producto.html')
-
-# Ahora (si cambiaste a catalogo.html)
-self.assertTemplateUsed(response, 'productos/catalogo.html')
